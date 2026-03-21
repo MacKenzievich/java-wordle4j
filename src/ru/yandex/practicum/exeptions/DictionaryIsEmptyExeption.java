68ad094f62fde725035c1506089decd1247e5266
mackenzy <mackenzievich@gmail.com>
2026-03-21 05:21:38 +0300

    Сделать исправления

--- src/ru/yandex/practicum/exeptions/DictionaryIsEmptyExeption.java
+++ src/ru/yandex/practicum/exeptions/DictionaryIsEmptyExeption.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.exeptions;
 
-public class DictionaryIsEmptyExeption extends Exception{
+public class DictionaryIsEmptyExeption extends Exception {
     public DictionaryIsEmptyExeption(String message) {
         super(message);
     }
